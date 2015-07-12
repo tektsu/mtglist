3854c2257b56db75bd58b424e0ec72244187ee01
Steve Baker <tektsu@gmail.com>
2015-07-12 10:03:01 -0700

    Command line processing

--- mtglist.js
+++ mtglist.js
@@ -1,15 +1,21 @@
 "use strict";
 
+require('pkginfo')(module)
+var cli = require('commander');
+
 var newEntry = require('./entry_factory');
 var newListContainer = require('./list_container_factory');
-// var MtgLists = require('./MtgLists');
 
 var cards = require('./AllCards-x.json');
 
-// var lists = new MtgLists();
+cli .version(module.exports.version)
+    .option('-o --directory [directory]', 'output directory')
+    .option('-s --source [source]', 'data source ("mtgjson" or a local file)')
+    .parse(process.argv);
+console.log(cli.directory);
+
 var lists = newListContainer();
 var names = Object.keys(cards);
-// console.log(names);
 names.forEach(processCard);
 
 function processCard(name) {
